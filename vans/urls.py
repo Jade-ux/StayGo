dcd04395e0e25b40782f91fa73a19ad924194238
Jade-ux <jadeemailsandbox@gmail.com>
2022-01-25 17:45:58 +0000

    Added van detail page, added view for van detail page and redirected the vans view to the first van in the database.

--- vans/urls.py
+++ vans/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.vans, name='vans'),
+    path('<int:van_id>/', views.van_detail, name='van_detail'),
 ]
--- vans/views.py
+++ vans/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, get_object_or_404, redirect)
+from .models import Van
 
 def vans(request):
     """This view returns the vans page"""
 
-    return render(request, 'vans/vans.html')
-    
+    vans = Van.objects.all()
+
+    context = {
+        'vans': vans,
+    }
+
+    # return render(request, 'vans/vans.html', context)
+    return redirect(van_detail, van_id = 1)
+
+
+def van_detail(request, van_id):
+    """ A view to show individual product details """
+
+    van = get_object_or_404(Van, pk=van_id)
+
+    context = {
+        'van': van,
+    }
+
+    return render(request, 'vans/van_detail.html', context)
