1795cf64de1dc745231fd4bb1e9471a448d601e1
Jade-ux <jadeemailsandbox@gmail.com>
2022-01-26 15:56:02 +0000

    Moved js base file into top of page to load before page content. On vans detail page, added content and removed date field, moved to base js file.

--- static/js/base.js
+++ static/js/base.js
@@ -5,7 +5,7 @@
         $('input[name="daterange"]').daterangepicker({
             opens: 'left'
         }, function (start, end, label) {
-            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end
+            console.log("New date selection made: " + start.format('YYYY-MM-DD') + ' to ' + end
                 .format('YYYY-MM-DD'));
         });
     });
