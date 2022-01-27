ee509bb3e16274e6f8698640302e0ee0f96f42d1
Jade-ux <jadeemailsandbox@gmail.com>
2022-01-27 07:28:08 +0000

    Fixed error in allowed hosts name.

--- staygoapp/settings.py
+++ staygoapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test1-staygo.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['test1-staygo.herokuapp.com', 'localhost']
 
 
 # Application definition
