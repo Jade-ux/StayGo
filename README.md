b9469ee85dcb9f08b0ce888132b441a7d4371bcb
Jade-ux <jadeemailsandbox@gmail.com>
2022-02-16 18:56:02 +0000

    Added add_to_cart view with date_from and date_to defined.

--- README.md
+++ README.md
@@ -34,3 +34,8 @@ You can now use the `heroku` CLI program - try running `heroku apps` to confirm 
 ## Features
 
 - Instagram feed - created with the API from Instagram: https://developers.facebook.com/docs/instagram-basic-display-api
+
+
+## Technologies used
+
+- Pandas - for data manipulation, specifically the date ranges provided.
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
 from datetime import datetime
+from django.shortcuts import render, get_object_or_404,redirect
+
 
 from vans.models import Van
 
@@ -15,8 +16,25 @@ def add_to_cart(request, item_id):
     
     van = get_object_or_404(Van, pk=item_id)
     # This will return the date range as a string
-    # In order to work with it I need it as a 
-    date_range = request.POST.get('daterange')
+    date_range_str = request.POST.get('daterange')
+    # This splits out the string into from and to dates and converts
+    # into datetime format
+    date_range_list = date_range_str.split(' - ')
+    date_from_str = date_range_list[0]
+    date_from = datetime.strptime(date_from_str, '%m/%d/%Y')
+    date_to_str = date_range_list[1]
+    date_to = datetime.strptime(date_to_str, '%m/%d/%Y')
+    # Gets the session to store the cart info
+    cart = request.session.get('cart', {})
 
-    return render(request, 'booking/cart.html')
+    # Puts the van into the cart with the date from and date to
+    # If the van exists in the cart already, it will be overwritten
+    # with the new dates
+    cart[item_id] = (date_from.isoformat(), date_to.isoformat())
+
+    # Updates the cart with the new details
+    request.session['cart'] = cart
+
+    # Takes user to Starsky booking form until there are more vans
+    return render(request, 'booking/booking.html')
     
